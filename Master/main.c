69e56ad7e4ab366f8fbc4f95bc84868710fd8d2f
TurkeyTitzOnIce <aidenrchristy03@gmail.com>
2025-04-24 20:24:30 -0600

    Added recieve stuff onto master
    realizing as I write this I didn't finish

--- Master/main.c
+++ Master/main.c
@@ -24,3 +24,35 @@ int main(void) {
         _delay_cycles(100000);
     }
 }
+
+
+#pragma vector = //instert interrupt flag here
+__interrupt void ISR_name(void){
+    position = 0;
+    UCA1IE |= UCTXCPTIE;
+    UCA1IFG &= ~UCTXCPTIFG;
+    UCA1TXBUF = message[position];
+
+    //clear flag
+
+}
+
+#pragma vector = EUSCI_A1_VECTOR
+__interrupt void ISR_EUSCI_A1(void){
+    if(UCA1IFG & UCTXIFG){
+        if(position == sizeof(message)){
+            UCA1IE &= ~UCTXCPTIE;
+        }else {
+            position++;
+            UCA1TXBUF = message[position];
+        }
+
+        UCA1IFG &= ~UCTXCPTIFG;
+    }
+
+    if(UCA1IFG & UCRXIFG){
+        //value = UCA1RXBUF
+
+        //Recieve clears on its own
+    }
+}
